69cfedd3a932a41ee22d1b726ac515fea81924e6
Nerixyz <nero.9@hotmail.de>
2020-04-13 20:57:04 +0200

    Updated nicovideo.js

--- parsers/nicovideo.js
+++ parsers/nicovideo.js
@@ -3,75 +3,148 @@ module.exports = (function (TemplateParser) {
 
 	const got = require("got");
 	const xmlParser = require("fast-xml-parser");
+	const htmlUnescape = require("unescape");
 
-	class NicovideoParser extends TemplateParser {
-		#url = "https://ext.nicovideo.jp/api/getthumbinfo/";
+	return class NicovideoParser extends TemplateParser {
+		#url = "https://www.nicovideo.jp/watch/";
 		#options = {};
 		#urlRegex = /nicovideo\.jp\/watch\/([s|n]m\d+)/;
 		#noUrlRegex = /(s|nm\d{7,9})/;
+		#envRegex = /data-environment="(.+)"/;
 
 		/**
-		 * Fetches XML data about a video based on its ID.
+		 * Fetches data about a video based on its ID.
 		 * @param {string} videoID
-		 * @returns {Promise<string>}
+		 * @returns {Promise<[{video, owner, thread, tags: Array}, string]>}
 		 */
-		#fetch = (videoID) => got(this.#url + videoID).json();
+		#fetch = async (videoID) => {
+			const url = this.#url + videoID;
+			const doc = await got(url);
+			const {playlistToken} = JSON.parse(htmlUnescape(doc.body.match(this.#envRegex)[1]));
+			// I'm probably doing something wrong here. Using got().json() or responseType: "json" returns 406 - Not Acceptable
+			const videoInfo = JSON.parse(await got(url, {
+				searchParams: {
+					mode: "pc_html5",
+					playlist_token: playlistToken,
+				},
+				resolveBodyOnly: true,
+			}));
+			const json = NicovideoParser.#createStreamRequestBody(videoInfo.video.dmcInfo.session_api);
+			const streamInfo = JSON.parse(await got("https://api.dmc.nico/api/sessions", {
+				method: "POST",
+				searchParams: {
+					_format: "json"
+				},
+				json,
+				headers: {"Content-Type": "application/json"},
+				resolveBodyOnly: true,
+			}));
+			return [videoInfo, streamInfo.data.session.content_uri];
+		};
 
-		constructor (options) {
+		constructor(options) {
 			super();
 			this.#options = options;
 		}
 
-		parseLink (link) {
+		parseLink(link) {
 			const match = link.match(this.#urlRegex);
 			return match ? match[1] : null;
 		}
 
-		checkLink (link, noURL) {
+		checkLink(link, noURL) {
 			if (noURL) {
 				return this.#noUrlRegex.test(link);
-			}
-			else {
+			} else {
 				return this.#urlRegex.test(link);
 			}
 		}
 
-		async checkAvailable (videoID) {
-			const xmlData = await this.#fetch(videoID);
-			const data = xmlParser.parse(xmlData).nicovideo_thumb_response;
-
-			return !data.error;
+		async checkAvailable(videoID) {
+			const doc = await got(this.#url + videoID);
+			return doc && doc.body.match(this.#envRegex);
 		}
 
-		async fetchData (videoID) {
-			const xmlData = await this.#fetch(videoID);
-			const rawData = xmlParser.parse(xmlData).nicovideo_thumb_response;
+		async fetchData(videoID) {
+			const [{video, owner, thread, tags}, link] = await this.#fetch(videoID);
 
-			if (rawData.error) {
-				return null;
-			}
 
-			const data = rawData.thumb;
 			return {
 				type: "nicovideo",
-				ID: data.video_id,
-				link: "https://www.nicovideo.jp/watch/" + data.video_id,
-				name: data.title,
-				author: data.user_nickname || null,
-				authorID: data.user_id,
-				description: data.description,
-				duration: data.length.split(":").map(Number).reduce((acc, cur, ind) => (ind === 0) ? acc += cur * 60 : acc += cur, 0),
-				created: new Date(data.first_retrieve),
-				views: data.view_counter || null,
-				comments: data.comment_num || null,
-				likes: data.mylist_counter || null,
-				thumbnail: data.thumbnail_url || null,
+				ID: video.id,
+				link: link,
+				// not sure if that's needed here
+				name: htmlUnescape(video.title),
+				author: owner.nickname || null,
+				authorID: owner.id,
+				description: htmlUnescape(video.description),
+				duration: video.duration,
+				created: new Date(video.postedDateTime),
+				views: video.viewCount || null,
+				comments: thread.commentCount || null,
+				likes: video.mylistCount || null,
+				// there's also video.largeThumbnailURL
+				thumbnail: video.thumbnailURL || null,
 				extra: {
-					tags: (data.tags) ? data.tags.tag :[]
+					tags: tags && tags.map(tag => tag.name) || [],
 				}
 			};
 		}
-	}
 
-	return NicovideoParser;
+		/**
+		 *
+		 * @param {{player_id, service_user_id, content_id, audios: Array, videos: Array, recipe_id, token, signature}} sessionApi
+		 * @returns {Object}
+		 */
+		static #createStreamRequestBody = (sessionApi) => ({
+			session: {
+				client_info: {
+					player_id: sessionApi.player_id,
+				},
+				content_auth: {
+					auth_type: "ht2",
+					content_key_timeout: 600000,
+					service_id: "nicovideo",
+					service_user_id: sessionApi.service_user_id,
+				},
+				content_id: sessionApi.content_id,
+				content_src_id_sets: [{
+					content_src_ids: [{
+						src_id_to_mux: {
+							audio_src_ids: sessionApi.audios,
+							// get the worst video stream to prevent buffering
+							video_src_ids: [sessionApi.videos[sessionApi.videos.length - 1]],
+						}
+					}]
+				}],
+				content_type: "movie",
+				content_url: "",
+				keep_method: {heartbeat: {lifetime: 120000}},
+				priority: 0,
+				protocol: {
+					name: "http",
+					parameters: {
+						http_parameters: {
+							parameters: {
+								hls_parameters: {
+									segment_duration: 6000,
+									transfer_preset: "",
+									use_ssl: "yes",
+									use_well_known_port: "yes",
+								},
+							},
+						},
+					},
+				},
+				recipe_id: sessionApi.recipe_id,
+				session_operation_auth: {
+					session_operation_auth_by_signature: {
+						token: sessionApi.token,
+						signature: sessionApi.signature,
+					}
+				},
+				timing_constraint: "unlimited"
+			}
+		});
+	}
 });
