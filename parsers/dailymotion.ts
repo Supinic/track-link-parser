683306701502fa977df814c589e78a59836eb4cc
Supinic <supinic@protonmail.com>
2024-05-14 22:24:07 +0200

    adjusted to eslint + typescript, removed `got`

--- parsers/dailymotion.ts
+++ parsers/dailymotion.ts
@@ -1,29 +1,8 @@
-import got from "got";
 import {
 	LinkParser,
 	Response,
 	ExtraData
 } from "./template.js";
-
-const urlRegex = /(dailymotion\.com\/video\/|dai\.ly\/)([kx][a-z0-9]{5,6})/;
-const noUrlRegex = /^[kx][a-z0-9]{5,6}$/;
-
-const dataFields = [
-	"created_time",
-	"description",
-	"duration",
-	"explicit",
-	"id",
-	"likes_total",
-	"owner",
-	"owner.screenname",
-	"private",
-	"tags",
-	"thumbnail_url",
-	"title",
-	"views_total",
-	"url"
-];
 
 export interface DailymotionData extends ExtraData {
 	explicit: boolean;
@@ -32,6 +11,27 @@ export interface DailymotionData extends ExtraData {
 export interface DailymotionResponse extends Response {
 	extra: DailymotionData;
 }
+
+const urlRegex = /(dailymotion\.com\/video\/|dai\.ly\/)([kx][a-z0-9]{5,6})/;
+const noUrlRegex = /^[kx][a-z0-9]{5,6}$/;
+const searchParams = new URLSearchParams({
+	fields: [
+		"created_time",
+		"description",
+		"duration",
+		"explicit",
+		"id",
+		"likes_total",
+		"owner",
+		"owner.screenname",
+		"private",
+		"tags",
+		"thumbnail_url",
+		"title",
+		"views_total",
+		"url"
+	].join(",")
+});
 
 type LocalDailymotionResponse = {
 	id: string;
@@ -47,11 +47,41 @@ type LocalDailymotionResponse = {
 	explicit: boolean;
 	tags: string[];
 };
+type LocalDailymotionCommentsResponse = {
+	page: number;
+	limit: number;
+	explicit: boolean;
+	total: number;
+	has_more: boolean;
+	list: string[];
+};
+
+const fetchVideoData = async (videoID: string) => {
+	try {
+		const response = await fetch(`https://api.dailymotion.com/video/${videoID}${searchParams}`);
+		const json = await response.json();
+		return json as LocalDailymotionResponse;
+	}
+	catch {
+		return null;
+	}
+};
+
+const fetchCommentsData = async (videoID: string) => {
+	try {
+		const response = await fetch(`https://api.dailymotion.com/video/${videoID}/comments`);
+		const json = await response.json();
+		return json as LocalDailymotionCommentsResponse;
+	}
+	catch {
+		return null;
+	}
+};
 
 export class DailymotionParser extends LinkParser {
 	readonly name = "dailymotion";
 
-	checkLink(link: string, noURL: boolean): boolean {
+	checkLink (link: string, noURL: boolean): boolean {
 		if (noURL) {
 			return noUrlRegex.test(link);
 		}
@@ -60,39 +90,25 @@ export class DailymotionParser extends LinkParser {
 		}
 	}
 
-	parseLink(url: string): string | null {
+	parseLink (url: string): string | null {
 		return url.match(urlRegex)?.[2] ?? null;
 	}
 
-	async checkAvailable(videoID: string): Promise<boolean> {
-		const { statusCode } = await got({
-			url: `https://api.dailymotion.com/video/${videoID}`,
-			throwHttpErrors: false
-		});
-
-		return (statusCode === 200);
+	async checkAvailable (videoID: string): Promise<boolean> {
+		const response = await fetch(`https://api.dailymotion.com/video/${videoID}`);
+		return (response.status === 200);
 	}
 
-	async fetchData(videoID: string): Promise<DailymotionResponse | null> {
-		let videoData;
-		let commentsData: any;
-		try {
-			[videoData, commentsData] = await Promise.all([
-				got({
-					url: `https://api.dailymotion.com/video/${videoID}`,
-					searchParams: {
-						fields: dataFields.join(",")
-					}
-				}).json(),
-				got(`https://api.dailymotion.com/video/${videoID}/comments`).json()
-			]);
+	async fetchData (videoID: string): Promise<DailymotionResponse | null> {
+		const [data, commentsData] = await Promise.all([
+			fetchVideoData(videoID),
+			fetchCommentsData(videoID)
+		]);
 
-		}
-		catch {
+		if (!data || !commentsData) {
 			return null;
 		}
 
-		const data = videoData as LocalDailymotionResponse;
 		return {
 			ID: data.id,
 			link: `https://dailymotion.com/${data.id}`,
